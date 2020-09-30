499b83e4b538501fc042d191921a45e6cffcc504
Shirlen Detablan <slen.detablan@gmail.com>
2020-09-30 17:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Your code here
 
 class Cat {
-  //...
+  consstructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+    speak () {
+      return `${this.name} says meow!`
+  }
 }
 
+
 class Dog {
-  //...
+  
 }
 
 class Bird {
